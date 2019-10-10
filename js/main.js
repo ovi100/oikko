2a539d0822f2c0c4a62ec9b0fc325af6a13c5122
ovi100 <ovi0167@hotmail.com>
2019-10-11 01:06:18 +0600

    oikko full done with responsive

--- js/main.js
+++ js/main.js
@@ -9,27 +9,35 @@
 $(document).ready(function() {
 
   $(window).scroll(function(){
-      if ($(this).scrollTop()>150){
-        $('.main-nav').addClass('fixed-nav');
-      } 
-      else {
-        $('.main-nav').removeClass('fixed-nav');
-      }
-    });
-
-  $('.body').on('shown.bs.collapse', function () {
-    $(this).parent().find('.fa-angle-down')
-                    .removeClass('fa-angle-down')
-                    .addClass('fa-angle-up');
-  }).on('hidden.bs.collapse', function () {
-      $(this).parent().find(".fa-angle-up")
-                      .removeClass("fa-angle-up")
-                      .addClass("fa-angle-down");
+    if ($(this).scrollTop()>150){
+      $('.main-nav').addClass('fixed-nav');
+    } 
+    else {
+      $('.main-nav').removeClass('fixed-nav');
+    }
   });
 
-  $('.carousel').carousel({
-    interval: 2000
-  })
+  $('.sidenav').click(function() {
+    /* Act on the event */
+    $('#menu-sidenav').toggleClass('show-sidemenu');
+  });
+
+  /*
+    * ----------------------------------------------
+    *----------Accordion with Toggle Icon-----------
+    * ----------------------------------------------
+  */
+
+  function toggleIcon(e){
+    $(e.target)
+      .prev('li a')
+      .find(".rotate-icon")
+      .toggleClass('icofont-thin-right icofont-thin-down');
+  }
+
+  $('.sidenav-menu-link').on('hidden.bs.collapse', toggleIcon);
+  $('.sidenav-menu-link').on('shown.bs.collapse', toggleIcon);
+
 
 
   /*
@@ -42,7 +50,7 @@ $(document).ready(function() {
   slider.vegas({
     overlay: false,
     cover:true,
-    transition: 'fade',
+    transition: ['fade','slideLeft'],
     transitionDuration: 4000,
     delay: 10000,
     timer:true,
